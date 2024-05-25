4ef43826010d3dd327901c71a5561bc1b72217db
alhasann351 <alhasann352@gmail.com>
2024-05-26 04:12:44 +0600

    login-screen-customize

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -26,182 +26,188 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           child: ListView(
             children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      AppStrings.loginTitle,
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: AppFontStyle.amaranth,
-                      ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    AppStrings.loginTitle,
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: AppFontStyle.amaranth,
                     ),
-                    const Text(
-                      AppStrings.loginSubtitle,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontFamily: AppFontStyle.amaranth,
-                      ),
+                  ),
+                  const Text(
+                    AppStrings.loginSubtitle,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: AppFontStyle.amaranth,
                     ),
-                    Center(
-                      child: Image.asset(
-                        ImageIconAssets.loginImage,
-                        width: 250,
-                        height: 250,
-                      ),
+                  ),
+                  Center(
+                    child: Image.asset(
+                      ImageIconAssets.loginImage,
+                      width: 250,
+                      height: 250,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               const SizedBox(
                 height: 15,
               ),
-              Expanded(
-                child: Center(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextFormField(
-                          cursorColor: AppColors.blackColor,
-                          style: const TextStyle(color: AppColors.blackColor),
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            prefixIcon: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Image.asset(ImageIconAssets.emailIcon, width: 30,),
+              Center(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        cursorColor: AppColors.blackColor,
+                        style: const TextStyle(color: AppColors.blackColor),
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(ImageIconAssets.emailIcon, width: 30,),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: AppStrings.emailHintText,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              style: BorderStyle.none,
                             ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintText: AppStrings.emailHintText,
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              style: BorderStyle.none,
                             ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        TextFormField(
-                          cursorColor: AppColors.blackColor,
-                          style: const TextStyle(color: AppColors.blackColor),
-                          keyboardType: TextInputType.text,
-                          obscureText: showPassword,
-                          decoration: InputDecoration(
-                            prefixIcon: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Image.asset(ImageIconAssets.passwordIcon, width: 30,),
-                            ),
-                            suffixIcon: GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  showPassword = !showPassword;
-                                });
-                              },
-                              child: Icon(
-                                showPassword
-                                    ? Icons.visibility_off_outlined
-                                    : Icons.visibility_outlined,
-                                color: AppColors.blackColor,
-                              ),
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintText: AppStrings.passwordHintText,
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        cursorColor: AppColors.blackColor,
+                        style: const TextStyle(color: AppColors.blackColor),
+                        keyboardType: TextInputType.text,
+                        obscureText: showPassword,
+                        decoration: InputDecoration(
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(ImageIconAssets.passwordIcon, width: 30,),
+                          ),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                showPassword = !showPassword;
+                              });
+                            },
+                            child: Icon(
+                              showPassword
+                                  ? Icons.visibility_off_outlined
+                                  : Icons.visibility_outlined,
+                              color: AppColors.blackColor,
                             ),
                           ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: AppStrings.passwordHintText,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              style: BorderStyle.none,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              style: BorderStyle.none,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
               const SizedBox(
                 height: 40,
               ),
-              Expanded(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: RoundedButton(
-                        title: AppStrings.login,
-                        backgroundColor: AppColors.buttonColor,
-                        textStyle: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: AppFontStyle.amaranth,
-                          color: AppColors.whiteColor,
-                        ),
-                        onTap: () {},
+              Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: RoundedButton(
+                      title: AppStrings.login,
+                      backgroundColor: AppColors.buttonColor,
+                      textStyle: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: AppFontStyle.amaranth,
+                        color: AppColors.whiteColor,
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        AppStrings.forgotPass,
+                        style: TextStyle(
+                            fontSize: 17, fontFamily: AppFontStyle.amaranth),
                       ),
                     ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: TextButton(
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: Image.asset(ImageIconAssets.phoneIcon, width: 30,),
+                    style: OutlinedButton.styleFrom(
+                        side: const BorderSide(
+                            width: 2, color: AppColors.commonColor)),
+                    label: const Text(
+                      AppStrings.loginPhoneNumber,
+                      style: TextStyle(
+                          fontSize: 18, fontFamily: AppFontStyle.amaranth),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        AppStrings.haveAccount,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontFamily: AppFontStyle.amaranth,
+                        ),
+                      ),
+                      TextButton(
                         onPressed: () {},
                         child: const Text(
-                          AppStrings.forgotPass,
+                          AppStrings.signup,
                           style: TextStyle(
-                              fontSize: 17, fontFamily: AppFontStyle.amaranth),
+                              fontSize: 17,
+                              fontFamily: AppFontStyle.amaranth),
                         ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    OutlinedButton.icon(
-                      onPressed: () {},
-                      icon: Image.asset(ImageIconAssets.phoneIcon, width: 30,),
-                      style: OutlinedButton.styleFrom(
-                          side: const BorderSide(
-                              width: 2, color: AppColors.commonColor)),
-                      label: const Text(
-                        AppStrings.loginPhoneNumber,
-                        style: TextStyle(
-                            fontSize: 18, fontFamily: AppFontStyle.amaranth),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          AppStrings.haveAccount,
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontFamily: AppFontStyle.amaranth,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            AppStrings.signup,
-                            style: TextStyle(
-                                fontSize: 17,
-                                fontFamily: AppFontStyle.amaranth),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
