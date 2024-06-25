5858ac001946ce1d9e769898e0b978888026d800
alhasann351 <alhasann352@gmail.com>
2024-06-25 20:34:34 +0600

    stream-builder-widget-separated

--- lib/view/notes_screen.dart
+++ lib/view/notes_screen.dart
@@ -12,6 +12,7 @@ import 'package:noteapp_firebase/resources/components/loading_animation_submit.d
 import 'package:noteapp_firebase/resources/fonts/app_font_style.dart';
 import 'package:noteapp_firebase/resources/routes/routes_name.dart';
 import 'package:noteapp_firebase/utils/app_util.dart';
+import 'package:noteapp_firebase/view/widgets/stream_builder_widget.dart';
 import 'package:noteapp_firebase/view_models/controller/theme_controller.dart';
 
 class NotesScreen extends StatefulWidget {
@@ -22,26 +23,10 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
-  final ThemeController themeController = Get.put(ThemeController());
   ExitDialog exitDialog = ExitDialog();
-
-  final dynamic _fireStore = FirebaseFirestore.instance
-      .collection('users')
-      .doc(FirebaseAuth.instance.currentUser!.uid)
-      .collection('notes')
-      .orderBy('id', descending: true)
-      .snapshots();
-
-  final CollectionReference _reference = FirebaseFirestore.instance
-      .collection('users')
-      .doc(FirebaseAuth.instance.currentUser!.uid)
-      .collection('notes');
-
-  bool flag = true;
 
   @override
   Widget build(BuildContext context) {
-    print('object');
     return PopScope(
       canPop: false,
       onPopInvoked: ((didPop) {
@@ -72,171 +57,12 @@ class _NotesScreenState extends State<NotesScreen> {
           ),
         ),
         drawer: AppDrawer(),
-        body: SafeArea(
+        body: const SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              StreamBuilder<QuerySnapshot>(
-                stream: _fireStore,
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return LoadingAnimationSubmit();
-                  }
-                  if (snapshot.hasError) {
-                    return AppUtil().showToastMessage('Error');
-                  }
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return const Center(child: Text('No notes found'));
-                  }
-                  return Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: GridView.builder(
-                        itemCount: snapshot.data!.docs.length,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 8.0,
-                          mainAxisSpacing: 8.0,
-                          mainAxisExtent: 280,
-                        ),
-                        itemBuilder: (context, index) {
-                          return Card(
-                            color: AppColors.cardBackgroundColor[
-                                index % AppColors.cardBackgroundColor.length],
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  flex: 2,
-                                  child: ListTile(
-                                    onTap: () {},
-                                    title: Text(
-                                      snapshot.data!.docs[index]['note-title']
-                                          .toString(),
-                                      maxLines: 1,
-                                      style: const TextStyle(
-                                        overflow: TextOverflow.ellipsis,
-                                        color: AppColors.whiteColor,
-                                        fontFamily: AppFontStyle.amaranth,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    subtitle: Padding(
-                                      padding: const EdgeInsets.only(
-                                          top: 4, bottom: 8),
-                                      child: Text(
-                                        snapshot
-                                            .data!.docs[index]['note-content']
-                                            .toString(),
-                                        maxLines: 7,
-                                        style: const TextStyle(
-                                          overflow: TextOverflow.ellipsis,
-                                          color: AppColors.whiteColor,
-                                          fontFamily: AppFontStyle.amaranth,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 1,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 15),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        GestureDetector(
-                                          onTap: (){
-                                            Get.toNamed(RoutesName.updateNotesScreen, arguments: {
-                                              'note-id': snapshot.data!.docs[index]['id'].toString(),
-                                              'note-title': snapshot.data!.docs[index]['note-title'].toString(),
-                                              'note-content': snapshot
-                                                .data!.docs[index]['note-content']
-                                                .toString(),
-                                            });
-                                          },
-                                          child: const CircleAvatar(
-                                            child: Icon(Icons.edit_note),
-                                          ),
-                                        ),
-                                        GestureDetector(
-                                          onTap: () {
-                                            _reference
-                                                .doc(snapshot
-                                                    .data!.docs[index]['id']
-                                                    .toString())
-                                                .delete()
-                                                .then((value) {
-                                              AppUtil().showToastMessage('Note deleted');
-                                            }).onError((error, stackTrace) {
-                                              AppUtil().showToastMessage(
-                                                  'Note not deleted');
-                                            });
-                                          },
-                                          child: const CircleAvatar(
-                                            child: Icon(Icons.delete_forever),
-                                          ),
-                                        ),
-                                        GestureDetector(
-                                            onTap: (){
-                                              if(flag){
-                                                _reference.doc(snapshot.data!.docs[index]['id'].toString()).update({
-                                                  'favorite': 'true',
-                                                }).then((value){
-                                                  AppUtil().showToastMessage('Note added in favorite');
-                                                }).onError((error, stackTrace){
-                                                  AppUtil().showToastMessage('Note not added in favorite');
-                                                });
-                                                flag = false;
-                                              }else{
-                                                _reference.doc(snapshot.data!.docs[index]['id'].toString()).update({
-                                                  'favorite': 'false',
-                                                }).then((value){
-                                                  AppUtil().showToastMessage('Note remove in favorite');
-                                                }).onError((error, stackTrace){
-                                                  AppUtil().showToastMessage('Note not remove in favorite');
-                                                });
-                                                flag = true;
-                                              }
-                                            },
-                                            child: CircleAvatar(
-                                              child: snapshot.data!.docs[index]['favorite']=='true'.toString() ?  const Icon(
-                                                Icons.favorite_rounded, color: Colors.red,) : const Icon(
-                                                Icons.favorite_border_rounded, color: Colors.white,),
-                                            ),),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    /*ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          onTap: () {},
-                          title: Text(snapshot.data!.docs[index]['note-title']
-                              .toString()),
-                          subtitle: Text(snapshot
-                              .data!.docs[index]['note-content']
-                              .toString()),
-                        );
-                      },
-                    ),*/
-                  );
-                },
-              ),
+              StreamBuilderWidget(),
             ],
           ),
         ),
